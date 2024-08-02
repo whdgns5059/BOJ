9d539b317dc8d1fabe08383036de376308546bf6
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2024-08-02 16:56:37 +0900

    P4344

--- src/com/my/backjoon/bronze/lv1/P4344.java
+++ src/com/my/backjoon/bronze/lv1/P4344.java
@@ -10,8 +10,35 @@ public class P4344 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int count = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < count ; i++){
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int number = Integer.parseInt(st.nextToken());
+
+            int[] points = new int[number];
+            int total = 0;
+
+            for(int j = 0 ; j < number ; j++){
+                points[j] = Integer.parseInt(st.nextToken());
+                total += points[j];
+            }
+
+            double avg = (double) total / number;
+
+            int upper = 0;
+            for(int j = 0 ; j < number ; j++){
+                if(avg < points[j]){
+                    upper++;
+                }
+            }
+
+            System.out.printf("%.3f%%\n", ((double)upper / number )* 100);
+
+        }
 
 
     }
